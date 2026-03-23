2754637b2eb5eaf3233fffd768a557a8f4b3e8e9
Justin Downer <justindowner121@gmail.com>
2026-03-23 19:02:26 -0400

    4bitMux Module made to replace RegDst mux

---
+++ Sourcecode/4BitMux.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/23/2026 06:31:19 PM
+// Design Name: 
+// Module Name: 4bitMux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fourbitMux(
+    input [3:0] input0,
+    input [3:0] input1,
+    input select,
+    output [3:0] out
+);
+
+assign out = select ? input1 : input0;
+    
+endmodule
