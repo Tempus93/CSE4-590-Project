a660801f396a2d1571922df06ea7b9103008ed3b
Justin Downer <justindowner121@gmail.com>
2026-03-23 13:34:27 -0400

    Forgot to paste XOR Code from Vivado

---
+++ Sourcecode/XORGate.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/23/2026 12:59:06 PM
+// Design Name: 
+// Module Name: XORGate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module XORGate(
+    input A,
+    input B,
+    output out
+    );
+    
+    assign out = A ^ B;
+endmodule
